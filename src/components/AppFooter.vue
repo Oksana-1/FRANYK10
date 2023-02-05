92cf44398265c7a9e5d2fbc640ca69ab3e210aab
LiliaBilous <liliyabilous.email@gmail.com>
2023-02-05 12:39:19 +0200

    added footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+<template>
+  <footer>
+    <div class="footer">
+      <div class="box_copyright">
+        Copyright © 2023   Сreated for educational purposes from open resources 
+      </div>
+      <div class="box_made-dy">
+        Made by
+        <a href="https://github.com/LiliaBilous" class="footer-ancor" target="_blank">Lilia Bilous</a>
+      </div>
+    </div>
+  </footer>
+</template>
