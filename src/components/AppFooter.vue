669b004130ac00ea71dee9afa88a05bde84d9058
LiliaBilous <liliyabilous.email@gmail.com>
2023-02-05 12:53:24 +0200

    added new color

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -3,11 +3,16 @@
   <footer>
     <div class="footer">
       <div class="box_copyright">
-        Copyright © 2023   Сreated for educational purposes from open resources 
+        Copyright © 2023 Сreated for educational purposes from open resources
       </div>
       <div class="box_made-dy">
         Made by
-        <a href="https://github.com/LiliaBilous" class="footer-ancor" target="_blank">Lilia Bilous</a>
+        <a
+          href="https://github.com/LiliaBilous"
+          class="footer-ancor"
+          target="_blank"
+          >Lilia Bilous</a
+        >
       </div>
     </div>
   </footer>
