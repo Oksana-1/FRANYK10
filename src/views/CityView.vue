6ce0e3140694f7b8d37ce184fa75fefd1c4b0484
LiliaBilous <liliyabilous.email@gmail.com>
2023-02-11 19:05:54 +0200

    add shadow to header/work with city page

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -4,10 +4,9 @@
       <div class="city__img-holder">
         <h2 class="city__header">Івано-Франківськ</h2>
         <div class="city__text-wrap">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-          quisquam enim ad, minima ipsa inventore nesciunt aut aliquam
-          perferendis itaque ut natus modi. Iure fugiat maxime delectus eius,
-          explicabo nam?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur quisquam
+          enim ad, minima ipsa inventore nesciunt aut aliquam perferendis itaque ut natus
+          modi. Iure fugiat maxime delectus eius, explicabo nam?
         </div>
         <div class="button-wrapper">
           <a href="" class="main-button">Перейти до огляду</a>
@@ -17,47 +16,40 @@
     <div class="secoundary-block">
       <h2 class="secoundary-header">Lorem ipsum dolor, sit amet consectetur</h2>
       <p class="text-wrap">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat,
-        culpa. Assumenda deleniti maiores eum nesciunt itaque voluptatum
-        corrupti exercitationem adipisci cupiditate nobis inventore, libero
-        incidunt! Eum nam quasi eligendi possimus. Aut quasi, ratione
-        perferendis unde porro nisi recusandae minima numquam debitis veritatis
-        neque quos placeat. Mollitia itaque explicabo similique tempore, culpa
-        sint quae temporibus! Excepturi veniam ipsum repellat labore voluptate.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat, culpa.
+        Assumenda deleniti maiores eum nesciunt itaque voluptatum corrupti exercitationem
+        adipisci cupiditate nobis inventore, libero incidunt! Eum nam quasi eligendi
+        possimus. Aut quasi, ratione perferendis unde porro nisi recusandae minima numquam
+        debitis veritatis neque quos placeat. Mollitia itaque explicabo similique tempore,
+        culpa sint quae temporibus! Excepturi veniam ipsum repellat labore voluptate.
       </p>
     </div>
     <div class="article-block-cafes">
-      <div class="article-wrapper">
-        <a class="ligth-asymetry-block" href="" target="">
-          <div class="img-holder">
-            <div class="franyk1"></div>
-          </div>
-          <div class="description">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-              voluptatem assumenda, quos dolores vitae similique ab at iure
-              incidunt enim quaerat, cumque labore fuga perspiciatis saepe
-              delectus debitis possimus? Dolor.
-            </p>
-            <span class="dark-button button">Button</span>
-          </div>
-        </a>
+      <div class="article-wrapper ligth-asymetry-block">
+        <div class="img-holder">
+          <img class="franyk1" src="../assets/img/pexels-cottonbro-studio-3205736.jpg" alt="caffe_1">
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et voluptatem
+            assumenda, quos dolores vitae similique ab at iure incidunt enim quaerat,
+            cumque labore fuga perspiciatis saepe delectus debitis possimus? Dolor.
+          </p>
+        </div>
+        <span class="dark-button button">Button</span>
       </div>
-      <div class="article-wrapper">
-        <a class="dark-asymetry-block" href="" target="">
-          <div class="img-holder">
-            <div class="franyk2"></div>
-          </div>
-          <div class="description">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab quia,
-              fugit error quo maxime in at consequatur quod veritatis earum
-              incidunt, veniam laudantium beatae nostrum inventore ipsa possimus
-              quidem? Qui?
-            </p>
-            <span class="main-button button">Button</span>
-          </div>
-        </a>
+      <div class="article-wrapper dark-asymetry-block">
+        <div class="img-holder">
+          <img class="franyk2" src="../assets/img/pexels-samer-daboul-1627933.jpg" alt="caffe_2">
+        </div>
+        <div class="description">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab quia, fugit error
+            quo maxime in at consequatur quod veritatis earum incidunt, veniam laudantium
+            beatae nostrum inventore ipsa possimus quidem? Qui?
+          </p>
+        </div>
+        <span class="main-button button">Button</span>
       </div>
     </div>
   </main>
