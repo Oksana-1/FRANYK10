cd549a38f751395a893073f835f7802bfa00c9fc
LiliaBilous <liliyabilous.email@gmail.com>
2023-02-27 16:24:31 +0200

    work with thml structure CafeView

--- src/views/CafeView.vue
+++ src/views/CafeView.vue
@@ -1,6 +1,27 @@
 <template>
-  <h1 class="cafe__title">{{ cafe.title }}</h1>
-  <p class="cafe-description">{{ cafe.text }}</p>
+  <div class="cafe-photo-wrap">
+    <div class="cafe__img-holder">
+      <h1 class="cafe__title">{{ cafe.title }}</h1>
+      <div class="rating"><span class="material-icons-outlined">e8d0</span></div>
+      <div class="cafe__comments">
+        <p></p>
+      </div>
+    </div>
+    <div class="cafe__contacts bottom-row">
+      <div class="cafe__contacts-icon">
+        <a href="#" class="cafe__cosial-link"></a>
+        <a href="#" class="cafe__cosial-link"></a>
+        <a href="#" class="cafe__cite-link"></a>
+      </div>
+      <div class="cafe__contacts-addres">{{ cafe.address }}</div>
+    </div>
+  </div>
+  <div class="cafe__img-holder small">
+    <div class="cafe__img"></div>
+    <p class="cafe__description">{{ cafe.text }}</p>
+    <h2 class="cafe__working-hours">{{ cafe.workHours }}</h2>
+  </div>
+  <div class="cafe__gallery"></div>
 </template>
 <script>
 import cafes from "../content/cafes.json";
