f0887252cf3de2a986bcba0cdb0a3f9994cea4db
LiliaBilous <liliyabilous.email@gmail.com>
2023-02-27 18:38:21 +0200

    add icons to CafeView Page

--- src/views/CafeView.vue
+++ src/views/CafeView.vue
@@ -2,24 +2,26 @@
   <div class="cafe-photo-wrap">
     <div class="cafe__img-holder">
       <h1 class="cafe__title">{{ cafe.title }}</h1>
-      <div class="rating"><span class="material-icons-outlined">e8d0</span></div>
+      <div class="rating">Рейтинг від сервісу GoogleMaps<i class="material-icons">star_rate</i>{{ cafe.rating }}</div>
+      <div class="cafe__price"><i class="material-icons">&#xe227;</i>{{ cafe.price }}</div>
       <div class="cafe__comments">
-        <p></p>
+        <p>{{ cafe.comments }}</p>
       </div>
     </div>
     <div class="cafe__contacts bottom-row">
       <div class="cafe__contacts-icon">
-        <a href="#" class="cafe__cosial-link"></a>
-        <a href="#" class="cafe__cosial-link"></a>
-        <a href="#" class="cafe__cite-link"></a>
+        <a href="#" class="cafe__cosial-link">Instagram</a>
+        <a href="#" class="cafe__cosial-link">Facebook</a>
+        <a href="#" class="cafe__cite-link">Web-cite</a>
       </div>
-      <div class="cafe__contacts-addres">{{ cafe.address }}</div>
+      <div class="cafe__contacts-addres"><i class="material-icons">location_on</i>{{ cafe.address }}</div>
+      <div class="cafe__contacts-telephone"><i class="material-icons">call</i>{{ cafe.telephone }}</div>
     </div>
   </div>
   <div class="cafe__img-holder small">
     <div class="cafe__img"></div>
     <p class="cafe__description">{{ cafe.text }}</p>
-    <h2 class="cafe__working-hours">{{ cafe.workHours }}</h2>
+    <h2 class="cafe__working-hours"><i class="material-icons">access_time</i>{{ cafe.workHours }}</h2>
   </div>
   <div class="cafe__gallery"></div>
 </template>
